c8a289ef8c9dc821d762ecfe6e0ba814fa63743c
TheZeldaBoi <63129353+Ldude162@users.noreply.github.com>
2021-09-27 09:20:37 -0700

    Update 1.1.4-6.py

--- Unit-1/1.1/Code/1.1.4-6.py
+++ Unit-1/1.1/Code/1.1.4-6.py
@@ -3,14 +3,14 @@ Made by Linus Reynolds
 On 9/23/2021
 1.1.4 #6
 '''
-breakLoop = True
 
-while(breakLoop):
+num1 = int(input("Please type in a number "))
+num2 = int(input("Type in another one. "))
+while(num1 % num2 != 0):
+    print("These numbers are not divisble! Please try again!")
     num1 = int(input("Please type in a number "))
     num2 = int(input("Type in another one. "))
+print("They are divisible!")
 
-    if num1 % num2 == 0:
-        print("They are divisible!")
-        breakLoop = False
-    else:
-        print("These numbers are not divisible! Try again!")
+
+    
