2c684fafceb675ace3c61f17d1eea96eec60f547
TheZeldaBoi <63129353+Ldude162@users.noreply.github.com>
2021-09-27 09:05:20 -0700

    removed break

--- Unit-1/1.1/Code/1.1.4-6.py
+++ Unit-1/1.1/Code/1.1.4-6.py
@@ -3,14 +3,14 @@ Made by Linus Reynolds
 On 9/23/2021
 1.1.4 #6
 '''
+breakLoop = True
 
-
-while(True):
+while(breakLoop):
     num1 = int(input("Please type in a number "))
     num2 = int(input("Type in another one. "))
 
     if num1 % num2 == 0:
         print("They are divisible!")
-        break
+        breakLoop = False
     else:
         print("These numbers are not divisible! Try again!")
