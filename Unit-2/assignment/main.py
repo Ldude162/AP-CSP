12e0db4dff894f65122bdb9979bbbab5cdb707f8
Ldude <63129353+Ldude162@users.noreply.github.com>
2021-12-14 14:03:39 -0800

    working on assignment

--- Unit-2/assignment/main.py
+++ Unit-2/assignment/main.py
@@ -2,22 +2,19 @@ f = open('data.txt', 'r')
 data = f.read()
 f.close()
 prev = 0
-f = open('temp.txt', 'a')
-for i in range(len(data)):
-    if i > 264:
-        f.write(data[i])
-f.close()
-f = open('temp.txt', 'r')
-newdata = f.read()
-f.close()
 split_strings = []
 n  = 3
-for index in range(0, len(newdata), n):
-    split_strings.append(newdata[index : index + n])
+for i in range(0, len(data), n):
+    split_strings.append(data[i : i + n])
 print(split_strings)
 
+index = 0
+num = 1
+gene = False
 for i in split_strings: 
-    if i == "TAG" or i == "ATG" or i == "TAA" or i == "TGA":
-        f = open('output.txt', 'a')
-        f.write(i + ' ')
-        f.close()
+    if i == 'ATG' and gene != True:
+        gene == True
+        f = open('positions.txt', 'a')
+        f.write('start ' + str(num) + ' is at')
+    elif i == 'TAG' or i == 'TAA' or i == 'TGA' and gene == True:
+        #enter code here
