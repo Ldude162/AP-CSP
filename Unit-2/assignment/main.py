4243c8e85667f558226cd11bec40da9d9c2c174e
Ldude <63129353+Ldude162@users.noreply.github.com>
2021-12-14 14:15:41 -0800

    working on assignment

--- Unit-2/assignment/main.py
+++ Unit-2/assignment/main.py
@@ -8,13 +8,19 @@ for i in range(0, len(data), n):
     split_strings.append(data[i : i + n])
 print(split_strings)
 
-index = 0
+index = 1
 num = 1
 gene = False
 for i in split_strings: 
     if i == 'ATG' and gene != True:
-        gene == True
+        gene = True
         f = open('positions.txt', 'a')
-        f.write('start ' + str(num) + ' is at')
+        f.write('start ' + str(num) + ' is at ' + str(index * 3 - 2) + '\n')
+        f.close()
     elif i == 'TAG' or i == 'TAA' or i == 'TGA' and gene == True:
-        #enter code here
+        gene = False
+        f = open('positions.txt', 'a')
+        f.write('end ' + str(num) + ' is at ' + str(index * 3 - 2) + '\n')
+        f.close()
+        num += 1
+    index += 1
