24abdd8fb592a6a879bd2e62ca82754e1cb06aa5
Ldude <63129353+Ldude162@users.noreply.github.com>
2021-12-15 12:43:32 -0800

    working on assignment

--- Unit-2/assignment/main.py
+++ Unit-2/assignment/main.py
@@ -1,28 +1,32 @@
+#read the data
 f = open('data.txt', 'r')
 data = f.read()
 f.close()
-prev = 0
-split_strings = []
-n  = 3
-for i in range(0, len(data), n):
-    split_strings.append(data[i : i + n])
-print(split_strings)
-
-index = 1
+newData = data[265:]
+ 
 num = 1
-gene = False
-for i in split_strings: 
-    if index * 3 - 2 > 265:
-        if i == 'ATG' and gene != True:
-            gene = True
-            f = open('positions.txt', 'a')
-            f.write('start ' + str(num) + ' is at ' + str(index * 3 - 2) + '\n')
-            f.close()
-        elif i == 'TAG' or i == 'TAA' or i == 'TGA':
-            if gene == True:
-                gene = False
+prevLetters = ['z', 'z']
+for i in range(len(newData)):
+    if prevLetters[0] == 'A' and prevLetters[1] == 'T' and newData[i] == 'G':
+        f = open('positions.txt', 'a')
+        f.write('start ' + str(num) + ' is at ' + str(i + 264) + '\n')
+        f.close()
+        newNewData = newData[i + 1:]
+        prev = 0
+        split_strings = []
+        n  = 3
+        for a in range(0, len(newData), n):
+            split_strings.append(newData[a : a + n])
+        index = 1
+        for b in split_strings:
+            if b == 'TAG' or b == 'TAA' or b == 'TGA':
                 f = open('positions.txt', 'a')
-                f.write('end ' + str(num) + ' is at ' + str(index * 3 - 2) + '\n')
+                f.write('end ' + str(num) + ' is at ' + str(index * 3 + 261 + i) + '\n')
                 f.close()
+                print(b)
                 num += 1
-    index += 1
+                i = index * 3 + 261 + i
+                break
+            index += 1
+    prevLetters[0] = prevLetters[1]
+    prevLetters[1] = newData[i]
