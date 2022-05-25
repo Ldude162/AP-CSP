c14ed8c470246d46d3eb346321746afae716194e
TheZeldaBoi <63129353+Ldude162@users.noreply.github.com>
2022-05-24 20:38:18 -0700

    working on ml project

--- ml/main.py
+++ ml/main.py
@@ -2,13 +2,22 @@ import pandas as pd
 
 data = pd.read_csv('./ml/data.csv', sep=',', header=None)
 
-training = data.loc[1:57, 4:14]
-testing = data.loc[1:57, 15:24]
+ratings = data.loc[1:57, 4:24]
 movies = data.loc[1:57, 2]
 people = data.loc[0, 4:24]
+
+movies = movies.to_frame()
+people = people.to_frame()
+
 print(people)
-def training(trainData, testData, movies, people):
-    data
+def trainingFunc(ratings, movies, people):
+    index = 4
+
+    for i in people.iterrows():
+        current = ratings.loc[1:57, index]
+        current = current.to_frame()
+        for a in current.iterrows():
+            print(a[1])
 
 
-training(training, testing, movies, people)
+trainingFunc(ratings, movies, people)
