b79476c32cd006029faea196019c0669bc46769e
TheZeldaBoi <63129353+Ldude162@users.noreply.github.com>
2022-05-24 17:57:56 -0700

    working on ml project

--- ml/main.py
+++ ml/main.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 import pandas as pd
 
 data = pd.read_csv('./ml/data.csv', sep=',', header=None)
@@ -8,5 +7,8 @@ testing = data.loc[1:57, 15:24]
 movies = data.loc[1:57, 2]
 people = data.loc[0, 4:24]
 print(people)
-def training(trainData, movies, people):
-    print(2)
+def training(trainData, testData, movies, people):
+    data
+
+
+training(training, testing, movies, people)
